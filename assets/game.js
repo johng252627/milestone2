0b0e2a907243df3d6c065a97fe398696cff5ae28
johng252627 <johngill252627@gmail.com>
2021-04-01 20:21:34 +0000

    Create array for images

---
+++ assets/game.js
@@ -0,0 +1,2 @@
+const cardDeck = ["anchor.png", "anchor.png", "arrow.png", "arrow.png", "bed.png", "bed.png", "bike.png", "bike.png",
+                "diamond.png", "diamond.png", "candy.png", "candy.png", "atom.png", "atom.png", "smile.png", "smile.png"];
