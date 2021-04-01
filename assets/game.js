74dd938d0d2bab12f383b2ecb197deab2fb25d0f
johng252627 <johngill252627@gmail.com>
2021-04-01 20:23:31 +0000

    Create constants for later use in game.js

--- assets/game.js
+++ assets/game.js
@@ -1,2 +1,12 @@
 const cardDeck = ["anchor.png", "anchor.png", "arrow.png", "arrow.png", "bed.png", "bed.png", "bike.png", "bike.png",
                 "diamond.png", "diamond.png", "candy.png", "candy.png", "atom.png", "atom.png", "smile.png", "smile.png"];
+
+const deck = document.querySelector(".deck-of-cards");
+
+const modal = document.getElementById("modal");
+
+const reset = document.querySelector(".reset-btn");
+
+const tryAgain = document.querySelector(".play-again-btn");
+
+const trackMoves = document.querySelector(".moves");
